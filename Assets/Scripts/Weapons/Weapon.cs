b9b001c33809c9fcaad782eb88beb934a07d4d1f
Rebecca Smith <2145912+Rebeccals@users.noreply.github.com>
2026-05-05 17:10:19 -0700

    Added particles and updated fps

--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -1,16 +1,33 @@
+using StarterAssets;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    StarterAssetsInputs starterAssetsInputs;
+
+    // Projectiles
+    [SerializeField] ParticleSystem muzzleFlash;
+
+    private void Awake() {
+        starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
+    }
+    
+    void Update() {
+        HandleShoot();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void HandleShoot() {
+        if (!starterAssetsInputs.shoot) return;
+
+        // Play muzzleFlash on everyshot
+        muzzleFlash.Play();
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity)){
+            Debug.Log(hit.collider.name);
+        }
         
+        starterAssetsInputs.ShootInput(false);
     }
 }
