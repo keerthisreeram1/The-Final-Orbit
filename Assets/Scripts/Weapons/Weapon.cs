b9a7af8780a1d5833a16001212dce0ce981cb796
Rebecca Smith <2145912+Rebeccals@users.noreply.github.com>
2026-05-10 15:11:31 -0700

    Added Screen Shake to each weapon

--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -1,15 +1,23 @@
 using UnityEngine;
+using Unity.Cinemachine;
 
 public class Weapon : MonoBehaviour
 {   
     [SerializeField] ParticleSystem muzzleFlash;
     [SerializeField] LayerMask shootableLayers;
 
+    CinemachineImpulseSource impulseSource;
+
+    void Awake(){
+        impulseSource = GetComponent<CinemachineImpulseSource>();
+    }
+
     public void Shoot(WeaponSO weaponSO, Camera mainCamera) {
         if (muzzleFlash) muzzleFlash.Play();
+        if (impulseSource) impulseSource.GenerateImpulse();
         
         RaycastHit hit;
-        
+
         if (Physics.Raycast(mainCamera.transform.position, mainCamera.transform.forward, out hit, Mathf.Infinity, shootableLayers, QueryTriggerInteraction.Ignore)) 
         {
             // Use the hit normal so the sparks/impact face the right way
