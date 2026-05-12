4c36f5c7a05f0dab3a9a547e017e79e354a8d8fa
Rebecca Smith <2145912+Rebeccals@users.noreply.github.com>
2026-05-12 01:07:13 -0700

    Added UI Health & Energy, Robot Chicken, Robot Spawners

---
+++ Assets/Scripts/PlayerEnergy.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerEnergy : MonoBehaviour
+{   
+    [Range(1, 10)]
+    [SerializeField] int maxEnergy = 10;
+    [SerializeField] Image[] energyBars;
+    int currentEnergy;
+
+    void Awake()
+    {
+        maxEnergy = Mathf.Min(maxEnergy, energyBars.Length);
+        currentEnergy = maxEnergy;
+        AdjustEnergyUI();
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentEnergy -= amount;
+        AdjustEnergyUI();
+
+        Debug.Log("Player Energy: " + currentEnergy);
+
+        if (currentEnergy <= 0)
+        {
+            Debug.Log("Player is dead!");
+        }
+    }
+
+    public int GetEnergyAmount()
+    {
+        return currentEnergy;
+    }
+
+    public void HealEnergy(int amount)
+    {
+        currentEnergy = Mathf.Min(maxEnergy, currentEnergy + amount);
+        AdjustEnergyUI();
+    }
+
+    void AdjustEnergyUI()
+    {
+        for(int i = 0; i < energyBars.Length; i++) {
+            if(i < currentEnergy){
+                energyBars[i].gameObject.SetActive(true);
+            } else {
+                energyBars[i].gameObject.SetActive(false);
+            }
+        }
+    }
+}
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,18 +1,25 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-    [SerializeField] int maxHealth = 100;
+    [Range(1, 10)]
+    [SerializeField] int maxHealth = 10;
+    [SerializeField] Image[] healthBars;
     int currentHealth;
 
     void Awake()
     {
+        maxHealth = Mathf.Min(maxHealth, healthBars.Length);
         currentHealth = maxHealth;
+        AdjustHealthUI();
     }
 
     public void TakeDamage(int amount)
     {
         currentHealth -= amount;
+        AdjustHealthUI();
+        
         Debug.Log("Player health: " + currentHealth);
 
         if (currentHealth <= 0)
@@ -21,8 +28,25 @@ public class PlayerHealth : MonoBehaviour
         }
     }
 
-    public int GetHealth()
+    public int GetHealthAmount()
     {
         return currentHealth;
+    }
+
+    public void Heal(int amount)
+    {
+        currentHealth = Mathf.Min(maxHealth, currentHealth + amount);
+        AdjustHealthUI();
+    }
+
+    void AdjustHealthUI()
+    {
+        for(int i = 0; i < healthBars.Length; i++) {
+            if(i < currentHealth){
+                healthBars[i].gameObject.SetActive(true);
+            } else {
+                healthBars[i].gameObject.SetActive(false);
+            }
+        } 
     }
 }
