c32109511c039edca7a65526e904ae3eb3476d81
Rebecca Smith <2145912+Rebeccals@users.noreply.github.com>
2026-05-13 11:16:54 -0700

    Added Win Condition

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -4,10 +4,17 @@ public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] GameObject robotExplosionVFX;
     [SerializeField] int startingHealth = 3;
+
     int currentHealth;
+    GameManager gameManager;
 
     void Awake() {
         currentHealth = startingHealth;
+    }
+
+    void Start() {
+        gameManager = FindFirstObjectByType<GameManager>();
+        gameManager.AdjustEnemiesLeft(1);
     }
 
     public void TakeDamage(int amount){
@@ -23,6 +30,7 @@ public class EnemyHealth : MonoBehaviour
     {
         if (robotExplosionVFX != null)
             Instantiate(robotExplosionVFX, transform.position, Quaternion.identity);
+        gameManager.AdjustEnemiesLeft(-1);
         Destroy(this.gameObject);
     }
     
--- Assets/Scripts/UI/GameManager.cs
+++ Assets/Scripts/UI/GameManager.cs
@@ -1,8 +1,29 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using StarterAssets;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] TMP_Text enemiesLeftText;
+    [SerializeField] GameObject youWinContainer;
+
+    int enemiesLeft = 0;
+    const string ENEMIES_LEFT_STRING = "Enemies Left: ";
+
+    public void AdjustEnemiesLeft(int amount)
+    {
+        enemiesLeft += amount;
+        enemiesLeftText.text = ENEMIES_LEFT_STRING + enemiesLeft.ToString();
+
+        if(enemiesLeft <= 0){
+            StarterAssetsInputs starterAssetsInputs = FindFirstObjectByType<StarterAssetsInputs>();
+            if (starterAssetsInputs != null)
+                starterAssetsInputs.SetCursorState(false);
+            youWinContainer.SetActive(true);
+        }
+    }
+
     public void RestartLevel()
     {
         int currentScene = SceneManager.GetActiveScene().buildIndex;
